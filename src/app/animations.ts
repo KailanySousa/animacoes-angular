774d8fc794c8cbc23548b03aaac8d8bd0db3e9b8
KailanySousa <kailany-sousa2010@hotmail.com>
2025-08-10 19:35:13 -0300

    feat: add estado coringa

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -27,10 +27,9 @@ export const highlightedState = trigger('highlightedState', [
 ]);
 
 export const shownStateTrigger = trigger('shownState', [
-  state('shown', style({})),
-  transition('void => shown', [
+  transition('void => *', [
     style({ opacity: 0 }),
     animate(300, style({ opacity: 1 })),
   ]),
-  transition('shown => void', [animate(300, style({ opacity: 0 }))]),
+  transition('* => void', [animate(300, style({ opacity: 0 }))]),
 ]);
