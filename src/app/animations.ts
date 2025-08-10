b67906e4b322e6a7c5ecb956199a5abb13ab890f
KailanySousa <kailany-sousa2010@hotmail.com>
2025-08-10 19:35:59 -0300

    refactor: substitui estado coringa por enter e leave na animação

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -27,9 +27,9 @@ export const highlightedState = trigger('highlightedState', [
 ]);
 
 export const shownStateTrigger = trigger('shownState', [
-  transition('void => *', [
+  transition(':enter', [
     style({ opacity: 0 }),
     animate(300, style({ opacity: 1 })),
   ]),
-  transition('* => void', [animate(300, style({ opacity: 0 }))]),
+  transition(':leave', [animate(300, style({ opacity: 0 }))]),
 ]);
